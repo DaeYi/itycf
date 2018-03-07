05c586a14fe7681d32e981c870dd8ebe5ab4995f
DaeYi <30500165+DaeYi@users.noreply.github.com>
2018-03-07 15:00:37 -0500

    Update main.js
    xml parser

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,10 @@ myHeading.textContent = 'Hello world!';
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("rawData").innerHTML = this.responseText;
+        parser = new DOMParser();
+        xmlDoc = parser.parseFromString(this.responseText, "text/xml");
+        
+        document.getElementById("rawData").innerHTML = xmlDoc.getElementsByTagName("id")[0].childNodes[0].nodeValue;
     }
 };
 xhttp.open("GET", "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%203%20and%20year(NEW_DATE)%20eq%202018", true);
