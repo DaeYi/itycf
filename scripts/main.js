add91eaea03f6df09eeac484957f00f6aa10e875
DaeYi <30500165+DaeYi@users.noreply.github.com>
2018-03-07 14:49:28 -0500

    Update main.js
    getting XML

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("rawdata").innerHTML = this.responseText;
+    }
+};
+xhttp.open("GET", "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%203%20and%20year(NEW_DATE)%20eq%202018", true);
+xhttp.send();
