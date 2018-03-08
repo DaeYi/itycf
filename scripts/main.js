052921c57d81b4a727d9258f7fd1387abb5f274b
DaeYi <30500165+DaeYi@users.noreply.github.com>
2018-03-08 14:06:27 -0500

    YQL reading

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,20 @@
 var myHeading = document.getElementById("headerID");
-myHeading.innerHTML = 'Hello world! v3';
-
-/***** DELETE THIS BLOCK IF YQL WORKS
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        parser = new DOMParser();
-        xmlDoc = parser.parseFromString(this.responseText, "text/xml");
-        
-        myHeading.textContent = "readyState";
-        
-        document.getElementById("rawData").innerHTML = xmlDoc.getElementsByTagName("id")[0].childNodes[0].nodeValue;
-    }
-};
-xhttp.open("GET", "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%203%20and%20year(NEW_DATE)%20eq%202018", true);
-xhttp.send();
-*/
+myHeading.innerHTML = 'Hello world! v4';
 
 var yqlCallback = function(data) {
     var rawData = document.getElementById("rawData")
+    
+    var entries = data.query.results.entry;
+    var num_entries = entries.length;
+    
+    var output = '';
+    for (var i = 0; i < num_entries; i++){
+        var id = entries[i].id;
+        var content = entries[i].content;
+        
+        output += id + ' - ' + content + '<br/>';
+    }
+    
     rawData.innerHTML = data.query.results.feed.title.content;
   };
 
